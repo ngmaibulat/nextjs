cf5d043c5b3012fe3cdb3a72466bfd064952875c
ngmAibulat <ngmaibulat@devbook.local>
2022-08-24 15:28:16 +0300

    index.js

--- pages/index.js
+++ pages/index.js
@@ -52,15 +52,6 @@ export default function Home(props)
         </ul>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
@@ -70,15 +61,6 @@ export default function Home(props)
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
