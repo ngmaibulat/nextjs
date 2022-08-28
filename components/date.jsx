66d306e37643864439d907d53ee23039b9730e40
ngm <ngmaibulat@gmail.com>
2022-08-28 16:01:47 +0300

    fetch git repo via ssh and analyze md files

--- components/date.jsx
+++ components/date.jsx
@@ -1,7 +1,22 @@
 import { parseISO, format } from 'date-fns';
 
-export default function Date({ dateString })
+export default function DateComponent(props)
 {
-    const date = parseISO(dateString);
-    return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+    let date;
+    let dt;
+
+    if (!props) {
+        date = new Date();
+        dt = format(date, 'LLLL d, yyyy');
+    }
+    else if ('dateString' in props && props.dateString) {
+        date = parseISO(props.dateString);
+        dt = format(date, 'LLLL d, yyyy');
+    }
+    else {
+        date = new Date();
+        dt = format(date, 'LLLL d, yyyy');
+    }
+
+    return <time dateTime={props.dateString}>{dt}</time>;
 }
