11aa746189248febb92eb336b13537182612d400
ngm <ngmaibulat@gmail.com>
2022-09-03 10:22:01 +0300

    pwa testing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextpwa = require('next-pwa');
+
+const pwaOptions = {
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: false,
+}
+
+const nextOptions = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     outputStandalone: true
   }
 }
+
+const withPWA = nextpwa(pwaOptions);
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA(nextOptions);
 
 module.exports = nextConfig
